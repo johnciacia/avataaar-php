7d47cf510d63b9c13b18c6713087a1a1ef578335
John Ciacia <john.ciacia@gmail.com>
2022-02-27 01:07:36 -0800

    add faker and laravel service providers

---
+++ src/AvataaarProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Avataaar;
+
+/**
+ * 
+ */
+class AvataaarProvider extends Faker\Provider\Base {    
+    
+    /**
+     * 
+     */
+    public function avataaar(): string {
+        return (new Avataaar())->url();
+    }
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Avataaar;
+
+use Faker\{Factory, Generator};
+
+/**
+ * 
+ */
+class ServiceProvider extends Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->singleton(Generator::class, function () {
+            $faker = Factory::create();
+            $faker->addProvider(new AvataaarProvider($faker));
+            return $faker;
+        });
+    }
+}
